34992430fea16a3527a4dd0965c07dc8e9369310
DSH132010 <80533223+DSH132010@users.noreply.github.com>
2021-08-04 20:53:44 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+
+
+window.alert("Click On The Camera To Take Snapshot !!");
+
 const cameraBtn = document.querySelector('.snap');
 
 
@@ -18,8 +22,6 @@ function setup() {
     video.hide();
 
 }
-
-window.alert("Click On The Camera To Take Snapshot !!");
 
 function draw() {
     image(img, 250, 170, 450, 420);
